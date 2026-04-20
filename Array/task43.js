57269afe2b1fa901d8e8f8f3402150014020a918
Uzair Ahmad <uzair247malik@gmail.com>
2026-04-20 17:48:58 +0500

    array

---
+++ Array/task43.js
@@ -0,0 +1,11 @@
+//Count how many numbers exist in an array.
+
+
+const arr = [2,4,7,8,9,10];
+
+ let count = 0;
+ for(let a in arr){
+    // console.log(a)
+    count++
+ }
+ console.log("the array no is ",count)
